9ff16df11807bdbf0b1c415d32a8a064569c0a0d
blukid <blukid@users.noreply.github.com>
2014-11-14 10:46:04 -0500

    new dummy file

---
+++ comma.asm
@@ -0,0 +1,38 @@
+	.DOSSEG
+DGROUP	GROUP	_DATA, STACK
+STACK	SEGMENT PARA STACK 'STACK'
+	DB	100h DUP (?)
+STACK	ENDS
+_DATA	SEGMENT WORD PUBLIC 'DATA'
+buffer	DB	100h DUP (0)
+_DATA	ENDS
+_TEXT	SEGMENT WORD PUBLIC 'CODE'
+	ASSUME	cs:_TEXT, ds:DGROUP, ss:DGROUP
+start:
+	mov	ax, DGROUP
+	mov	ds, ax
+	mov	di, OFFSET buffer
+	mov	si, 82h
+	mov	al, es:[80h]
+	cmp	al, 0
+	je	done
+	
+loop1:
+	mov	al, es:[si]
+	mov	[di], al
+	inc	di
+	inc	si
+next:
+	mov	al, 10
+	mov	[di], al
+	inc	di
+	mov	al, '$'
+	mov	[di], al
+	mov	ah, 9h
+	mov	dx, OFFSET buffer
+	int	21h
+done:
+	mov	ax, 4C00h
+	int	21h
+_TEXT	ENDS
+	END	start
